0bad82439d5945dd2d7439acda42ad62539658e3
Mark Birbeck <mark.birbeck@gmail.com>
2015-09-17 15:28:25 +0100

    use highland pipelines to define tasks

--- index.js
+++ index.js
@@ -23,16 +23,17 @@ var addRecipe = function(gulp, recipe, connections) {
   var src = channels[_if.channel].src;
   var dest = channels[_then.channel].dest;
 
-  gulp.task(recipe.name, function() {
-    var stream = h(src(_if.glob, _if.opts));
-
-    if (recipe.map) {
-      stream = stream
-        .map(recipe.map)
-    }
-
-    return stream
-      .pipe(dest(_then.glob, _then.opts));
+  gulp.task(recipe.name, function(cb) {
+    return h.pipeline(function(stream) {
+      return stream
+        .pipe(src(_if.glob, _if.opts))
+        .map(function(file) {
+          return (recipe.map) ? recipe.map(file) : file;
+        })
+        .pipe(dest(_then.glob, _then.opts))
+        .done(cb)
+        ;
+    });
   });
 };
 
