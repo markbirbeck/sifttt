e4670526aca4538b92a523d3b253d03b736efaf2
Mark Birbeck <mark.birbeck@gmail.com>
2015-11-18 14:16:12 +0000

    factor channels so that we can get recursive

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-exports.addRecipe = require('./lib/addRecipe');
+var addRecipe = require('./lib/addRecipe');
+
+module.exports = function(gulp, connections, recipes) {
+  var channels = require('./lib/channels')(connections, recipes);
+
+  recipes.forEach(function(recipe) {
+    addRecipe(gulp, recipe, connections, channels);
+  });
+}
