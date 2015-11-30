aa64d9fff8a84509192b0b5857816087fe59b735
Mark Birbeck <mark.birbeck@gmail.com>
2015-11-30 16:19:32 +0000

    allow CLI options to override recipe settings

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+var minimist = require('minimist');
 var addRecipe = require('./lib/addRecipe');
+
+/**
+ * Pass any command-line options in to addRecipe():
+ */
+
+var opts = minimist(process.argv.slice(2));
 
 module.exports = function(gulp, connections, recipes) {
   var channels = require('./lib/channels')(connections, recipes);
 
   recipes.forEach(function(recipe) {
-    addRecipe(gulp, recipe, connections, channels);
+    addRecipe(gulp, recipe, connections, channels, opts);
   });
 }
---
+++ test/cliOptions.js
@@ -0,0 +1,62 @@
+var path = require('path');
+require('chai').should();
+var uut = require('../lib/streamFromRecipe');
+
+var recipe = {
+  name: 'read and write',
+  input: {
+    channel: 'gulp',
+    glob: path.join(__dirname, 'fixtures', 'file.json')
+  },
+  output: {
+    channel: 'gulp',
+    glob: path.join(__dirname, 'fixtures', 'output')
+  }
+};
+var connections = {};
+var channels = require('../lib/channels')(connections, [recipe]);
+
+describe('command-line options', function() {
+  it('should work without options', function(done) {
+    uut(recipe, connections, channels)
+    .toArray(function(fileList) {
+      fileList.should.have.length(1);
+      fileList.forEach(function(file) {
+        var data = JSON.parse(String(file.contents));
+
+        data.should.eql({'hello': 'world'});
+      });
+      done();
+    })
+    ;
+  });
+
+  it('should work with empty options', function(done) {
+    uut(recipe, connections, channels)
+    .toArray(function(fileList) {
+      fileList.should.have.length(1);
+      fileList.forEach(function(file) {
+        var data = JSON.parse(String(file.contents));
+
+        data.should.eql({'hello': 'world'});
+      });
+      done();
+    })
+    ;
+  });
+
+  it('option to override glob', function(done) {
+    uut(recipe, connections, channels,
+      {'input.glob': path.join(__dirname, 'fixtures', 'file2.json')})
+    .toArray(function(fileList) {
+      fileList.should.have.length(1);
+      fileList.forEach(function(file) {
+        var data = JSON.parse(String(file.contents));
+
+        data.should.eql({'hello': 'there'});
+      });
+      done();
+    })
+    ;
+  });
+});
