95f9f874f2be79eae570869321bf9e236dee07c6
Mark Birbeck <mark.birbeck@gmail.com>
2015-12-02 11:19:58 +0000

    add test for getMessageCount()

---
+++ test/queueCount.js
@@ -0,0 +1,39 @@
+var path = require('path');
+require('chai').should();
+var uut = require('../lib/streamFromRecipe');
+
+var recipes = [
+  {
+    input: {
+      channel: 'amqp',
+      glob: process.env.SQS_QUEUE_NAME,
+      opts: {
+        getMessageCount: true
+      }
+    },
+    output: {
+      channel: 'gulp',
+      glob: path.join(__dirname, 'fixtures', 'output')
+    }
+  }
+];
+var connections = {};
+
+var channels = require('../lib/channels')(connections, recipes);
+
+describe('amqp channel', function() {
+  it('should get queue count', function(done) {
+    uut(recipes[0], connections, channels)
+    .toArray(function(fileList) {
+      fileList.should.have.length(1);
+      fileList.forEach(function(file) {
+        var data = JSON.parse(String(file.contents));
+
+        data
+        .should.be.a('number');
+      });
+      done();
+    })
+    ;
+  });
+});
