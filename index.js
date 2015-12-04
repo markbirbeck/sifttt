20aec9a6312568a958b620161624f343c31cf501
Mark Birbeck <mark.birbeck@gmail.com>
2015-12-04 17:56:07 +0000

    spawn child processes for default task dependencies

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+'use strict';
+
+let spawn = require('child_process').spawn;
 var minimist = require('minimist');
 var addRecipe = require('./lib/addRecipe');
 var createApi = require('./lib/api');
@@ -8,7 +11,7 @@ var createApi = require('./lib/api');
 
 var opts = minimist(process.argv.slice(2));
 
-module.exports = function(gulp, connections, recipes) {
+module.exports = function(gulp, connections, recipes, defaultTaskDependencies) {
   var channels = require('./lib/channels')(connections, recipes);
 
   recipes.forEach(function(recipe) {
@@ -20,4 +23,44 @@ module.exports = function(gulp, connections, recipes) {
    */
 
   gulp.task('api', createApi(recipes, connections, channels));
+
+  /**
+   * If there is a list of dependencies for the default task then create
+   * a default task which calls out to those dependencies:
+   */
+
+  if (!opts.spawn && defaultTaskDependencies) {
+    let tasks = defaultTaskDependencies
+      .split(',')
+      .map(taskName => taskName.trim())
+      ;
+
+    console.log(`Creating default task with ${tasks}`);
+
+    gulp.task('default', cb => {
+      let taskList = [];
+
+      tasks.forEach(taskName => {
+        let task = spawn('gulp', [taskName, '--spawn']);
+
+        task.stdout.on('data', data => {
+          console.log(`stdin for task '${taskName}': ${data}`);
+        });
+
+        task.stderr.on('data', data => {
+          console.error(`stderr for task '${taskName}': ${data}`);
+        });
+
+        task.on('close', code => {
+          if (code !== 0) {
+            console.error(`task '${taskName}' exited: ${code}`);
+          } else {
+            console.log(`task '${taskName}' exited successfully`);
+          }
+        });
+
+        taskList.push(task);
+      })
+    });
+  }
 }
