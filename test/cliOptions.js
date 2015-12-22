d071389eac557f0b705e9e925de19add601e5874
Mark Birbeck <mark.birbeck@gmail.com>
2015-12-22 15:54:05 +0000

    align cli option override test

--- test/cliOptions.js
+++ test/cliOptions.js
@@ -47,7 +47,7 @@ describe('command-line options', function() {
 
   it('option to override glob', function(done) {
     uut(recipe, connections, channels,
-      {'input.glob': path.join(__dirname, 'fixtures', 'file2.json')})
+      `input.glob=${path.join(__dirname, 'fixtures', 'file2.json')}`)
     .toArray(function(fileList) {
       fileList.should.have.length(1);
       fileList.forEach(function(file) {
