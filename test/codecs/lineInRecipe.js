9a5d6cb84338cdc1e1f1a0cb6a8931642f05fa28
Mark Birbeck <mark.birbeck@gmail.com>
2016-04-20 13:28:35 +0100

    [#21] restructure streamFromRecipe() to apply codecs on each input
    Fixes #21.

---
+++ test/codecs/lineInRecipe.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * Test the 'line' codec in a recipe:
+ */
+
+var path = require('path');
+require('chai').should();
+let uut = require('../../lib/streamFromRecipe');
+
+let recipes = [
+  {
+    name: 'read',
+    input: {
+      channel: 'gulp',
+      glob: [
+        path.join(__dirname, '../fixtures/file.csv'),
+        path.join(__dirname, '../fixtures/file2.csv')
+      ],
+      opts: { buffer: false },
+      codec: {
+        line: {
+          drop: 1
+        }
+      }
+    },
+    output: {
+      channel: 'noop'
+    },
+    filter: [
+      {
+        csv: {
+          columns: [
+            'greeting'
+          ]
+        }
+      }
+    ]
+  }
+];
+
+let connections = {};
+let codecs = require('../../lib/codecs');
+let channels = require('../../lib/channels')(connections, codecs, recipes);
+
+describe('line codec in a recipe', function() {
+  it('should read', function(done) {
+    uut(recipes[0], connections, codecs, channels)
+    .collect()
+    .doto(ar => {
+      ar
+      .map(file => JSON.parse(String(file.contents)))
+      .should.eql([
+        {'greeting': 'hello'},
+        {'greeting': 'bonjour'},
+        {'greeting': 'howdy'},
+
+        {'greeting': 'wotcha'},
+        {'greeting': 'bona sera'}
+      ]);
+    })
+    .done(done)
+    ;
+  });
+});
