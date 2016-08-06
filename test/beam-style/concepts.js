352fde0273eddb0d4e30e8661612661b9e7573a0
Mark Birbeck <mark.birbeck@gmail.com>
2016-08-06 16:58:50 +0100

    add nested pipeline, using through()

--- test/beam-style/concepts.js
+++ test/beam-style/concepts.js
@@ -30,3 +30,44 @@ describe('basic pipeline', () => {
     ;
   });
 });
+
+describe('nested pipeline', () => {
+  it('using through() with stream as parameter', () => {
+
+    /**
+     * Create a pipeline that doubles the elements and adds 3:
+     */
+
+    let p = s => s
+    .map(element => {
+      return element * 2;
+    })
+    .map(element => {
+      return element + 3;
+    })
+    ;
+
+    /**
+     * Triple some elements, run them through the pipeline, and then
+     * subtract 2:
+     */
+
+    h([1, 2, 3, 4])
+    .map(element => {
+      return element * 3;
+    })
+    .through(p)
+    .map(element => {
+      return element - 2;
+    })
+    .toArray(ar => {
+      ar.should.eql([
+        (((1 * 3) * 2) + 3) - 2,
+        (((2 * 3) * 2) + 3) - 2,
+        (((3 * 3) * 2) + 3) - 2,
+        (((4 * 3) * 2) + 3) - 2
+      ]);
+    })
+    ;
+  });
+});
