11c887e0a1287f2c2ac4431f06cb449dc2f430db
Mark Birbeck <mark.birbeck@gmail.com>
2016-08-07 16:58:48 +0100

    implement Pipeline, InputCollection and some core transforms according to 'archictecture' tests

---
+++ test/beam/Pipeline.js
@@ -0,0 +1,26 @@
+'use strict';
+require('chai').should();
+
+const Pipeline = require('../../lib/beam/Pipeline');
+const InputCollection = require('../../lib/beam/InputCollection');
+const ct = require('../../lib/beam/coreTransforms');
+
+describe('Pipeline', () => {
+  it('core transforms', done => {
+    let p = new Pipeline()
+    .apply(new InputCollection([4, 3, 2, 1]))
+    .apply(new ct.Map(element => element - 8))
+    .apply(new ct.Collect())
+    .apply(new ct.DoTo(ar => {
+      ar.should.eql([
+        4 - 8,
+        3 - 8,
+        2 - 8,
+        1 - 8
+      ]);
+    }))
+    ;
+
+    p.run(done);
+  });
+});
