a3607fffd1bc8df45eee302cd7b290d7c53f23b7
Mark Birbeck <mark.birbeck@gmail.com>
2016-08-07 12:40:16 +0100

    add test for calling a nested pipeline and discarding results

--- test/beam-style/concepts.js
+++ test/beam-style/concepts.js
@@ -124,4 +124,79 @@ describe('nested pipeline', () => {
     .done(done)
     ;
   });
+
+  it('call pipeline and ignore results', (done) => {
+
+    /**
+     * Create a function that creates a pipeline that triples the
+     * elements and adds 7:
+     */
+
+    let transforms = [
+      h.map(element => {
+        return element * 3;
+      }),
+      h.map(element => {
+        return element + 7;
+      })
+    ];
+
+    let pipeline = s => transforms.reduce((stream, fn) => fn(stream), s);
+
+    /**
+     * Create a function that will call a pipeline but then continue the
+     * previous pipeline:
+     */
+
+    let callPipelineAndIgnoreResults = p => {
+      return s => {
+        return s = s.consume((err, data, push, next) => {
+          if (err) {
+            push(err);
+            next();
+            return;
+          }
+
+          if (data === h.nil) {
+            push(null, h.nil);
+            return;
+          }
+
+          h([data])
+          .through(p)
+          .done(() => {
+            push(null, data);
+            next();
+          })
+          ;
+        });
+      };
+    };
+
+    /**
+     * Quadruple some elements, run them through the pipeline, and then
+     * subtract 20; the results we're expecting should NOT include any results
+     * from the pipeline:
+     */
+
+    h([2, 3, 7, 6])
+    .map(element => {
+      return element * 4;
+    })
+    .through(callPipelineAndIgnoreResults(pipeline))
+    .map(element => {
+      return element - 20;
+    })
+    .collect()
+    .doto(ar => {
+      ar.should.eql([
+        (2 * 4) - 20,
+        (3 * 4) - 20,
+        (7 * 4) - 20,
+        (6 * 4) - 20
+      ]);
+    })
+    .done(done)
+    ;
+  });
 });
