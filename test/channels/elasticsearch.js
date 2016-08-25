b45f93f43f8e88090234cde3ce33441af19ab075
Mark Birbeck <mark.birbeck@gmail.com>
2016-08-25 15:21:08 +0100

    increase timeout value on ES channel test

--- test/channels/elasticsearch.js
+++ test/channels/elasticsearch.js
@@ -49,6 +49,7 @@ var channels = require('../../lib/channels')(connections, recipes);
 
 describe('elasticsearch channel', function() {
   it('should query', function(done) {
+    this.timeout(5000);
     uut(recipes[0], connections, codecs, channels)
     .toArray(function(fileList) {
       fileList.forEach(function(file) {
