6c6ab67a579e882980bc6e85ab1abf562e7b58c4
Mark Birbeck <mark.birbeck@gmail.com>
2016-10-27 14:34:31 +0100

    add Otherwise transform for use when a stream is empty

--- test/beam/Pipeline.js
+++ test/beam/Pipeline.js
@@ -23,4 +23,23 @@ describe('Pipeline', () => {
 
     p.run(done);
   });
+
+  it('Otherwise', done => {
+    let p = Pipeline.create()
+    .apply(new InputCollection([]))
+    .apply(new ct.Otherwise([8, 7, 6, 5]))
+    .apply(new ct.Map(element => element - 4))
+    .apply(new ct.Collect())
+    .apply(new ct.DoTo(ar => {
+      ar.should.eql([
+        8 - 4,
+        7 - 4,
+        6 - 4,
+        5 - 4
+      ]);
+    }))
+    ;
+
+    p.run(done);
+  });
 });
