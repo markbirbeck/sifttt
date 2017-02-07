b4e45df4be918a0348d79febb392f3302a1457e0
Mark Birbeck <mark.birbeck@gmail.com>
2017-02-07 10:52:40 +0000

    support using placeholders and parameters in MySQL queries

--- test/beam/MySqlSource.js
+++ test/beam/MySqlSource.js
@@ -67,4 +67,62 @@ describe('MySqlSource', () => {
 
     p.run(done);
   });
+
+  it.skip('use parameters', done => {
+    let p = Pipeline.create()
+    .apply(Read.from(new MySqlSource(
+      `
+        SELECT
+          t2.*,
+          t3.*
+        FROM
+          state51__Base t2,
+          state51__GreedOrder t3,
+          state51__Base t4,
+          state51__GreedOrderLine t5,
+          state51__Base t6,
+          state51__StockQuote t7
+        WHERE
+          t2.id = t3.id
+          AND
+          (
+            t5.stockquote = t6.id
+            AND
+            t5.greedorder = t2.id
+            AND
+            t3.status = ?
+            AND
+            t7.source = ?
+            AND
+            t3.transaction_time >= ?
+          )
+          AND
+          t7.id = t6.id
+          AND
+          t6._type IN (142)
+          AND
+          t5.id = t4.id
+          AND
+          t4._type IN (141, 275)
+        LIMIT 10
+      `,
+      ['complete', 1396680, 2016],
+      {
+        host: process.env.DBHOST_READONLY,
+        user: process.env.DBUSER,
+        password: process.env.DBPASS,
+        database: process.env.DATABASE
+      }
+    )))
+    .apply(new ct.DoTo(element => {
+      element.should.have.property('id');
+    }))
+    .apply(new ct.Collect())
+    .apply(new ct.DoTo(element => {
+      element.should.have.length(10);
+    }))
+    ;
+
+    p.run(done);
+  });
 });
